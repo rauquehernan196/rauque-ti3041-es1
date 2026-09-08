b17d822469e9ebb31776a4502c89021dcfae1409
rauquehernan196 <rauquehernan196@gmail.com>
2026-09-08 01:07:09 -0300

    etapa-2-interfaz

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.lista_productos, name='lista'),
+    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,7 +1,22 @@
+import json
+import os
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
-from django.http import HttpResponse
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(BASE_DIR, 'datos.json')
 
-def home(request):
-    return HttpResponse("Página principal de catálogo")
+def cargar_datos():
+    with open(JSON_PATH, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def lista_productos(request):
+    productos = cargar_datos()
+    return render(request, 'catalogo/lista.html', {'productos': productos})
+
+def detalle_producto(request, producto_id):
+    productos = cargar_datos()
+    producto = next((p for p in productos if p['id'] == producto_id), None)
+    if not producto:
+        raise Http404("El producto no existe en el catálogo de ferretería.")
+    return render(request, 'catalogo/detalle.html', {'producto': producto})
